4f6f74639afb7ebfbea3ff30b89e70d41c156ce2
☁️☁️☁️☁️☁️☁️ <106909332+starMinK@users.noreply.github.com>
2023-08-17 09:33:44 +0900

    Revert "최종 메인 업로드"

--- cheongchun28/src/test/java/com/example/cheongchun28/Cheongchun28ApplicationTests.java
+++ cheongchun28/src/test/java/com/example/cheongchun28/Cheongchun28ApplicationTests.java
@@ -1,8 +1,9 @@
 package com.example.cheongchun28;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class Cheongchun28ApplicationTests {
 
 	@Test
